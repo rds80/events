14043d66f7924361ed47735c64f7a480fc9a121c
Shah, Ripple <Ripple.Shah@tdsecurities.com>
2017-06-01 17:14:14 -0400

    Added ng2-toastr css and js files to index.html

--- src/common/toastr.service.ts
+++ src/common/toastr.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { ToastsManager } from "ng2-toastr/ng2-toastr";
 
 declare let toastr: any
 
 @Injectable()
 export class ToastrService {
+    constructor(public toastr: ToastsManager) {
+        
+    }
+
     success(message: string, title?: string) {
         toastr.success(message, title);
     }
