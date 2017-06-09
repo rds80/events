eef5734a03dc1ccecded5eb541c3f4e4e5b36006
Shah, Ripple <Ripple.Shah@tdsecurities.com>
2017-06-09 17:09:27 -0400

    Added Search bar filter

--- src/nav-bar/nav-bar.component.ts
+++ src/nav-bar/nav-bar.component.ts
@@ -1,5 +1,7 @@
+import { ISession } from 'events/shared/index';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "user/auth.service";
+import { EventService } from "../events/index";
 
 @Component({
   selector: 'nav-bar',
@@ -7,10 +9,20 @@ import { AuthService } from "user/auth.service";
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  searchTerm: string = "";
+  foundSessions: ISession[];
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private eventService: EventService) { 
+  }
 
   ngOnInit() {
+  }
+
+  searchSessions(searchTerm) {
+    this.eventService.searchSessions(searchTerm).subscribe( sessions => {
+      this.foundSessions = sessions;
+      console.log(this.foundSessions);
+    })
   }
 
 }
